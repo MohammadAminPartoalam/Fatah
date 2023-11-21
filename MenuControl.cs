f11f98e118ee4a3d9895b17e231d3c2fe1c96788
MohammadAminPartoalam <151610617+MohammadAminPartoalam@users.noreply.github.com>
2023-11-21 10:39:26 +0330

    Add files via upload

---
+++ MenuControl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour
+{
+    public void Click_Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Click_Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Click_Next_Level()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+}
---
+++ Player1.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+    public float Speed;
+    public GameObject Menu;
+    AudioSource Sound;
+    float horizontalInput;
+    public GameObject Win;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Mouse X");
+
+        if(Input.GetMouseButton(0))
+        {
+            transform.Translate(new Vector3(horizontalInput * Speed * Time.deltaTime, 0f, 0f));
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D tagsPlayer)
+    {
+        if (tagsPlayer.tag == "Barriers")
+        {
+            Destroy(gameObject);
+            Menu.SetActive(true);
+        }
+        
+        if(tagsPlayer.tag == "Win")
+        {
+            Win.SetActive(true);
+        }
+    }
+}
